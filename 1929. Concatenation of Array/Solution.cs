6c7a8e15e94056f6a74aa2c609a78006e0720a6a
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-04-22 19:31:00 +0300

    Initial commit

---
+++ 1929. Concatenation of Array/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution 
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] concatenationArray = new int[nums.Length * 2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            concatenationArray[i] = nums[i];
+            concatenationArray[nums.Length + i] = nums[i];
+        }
+
+        return concatenationArray;
+    }
+}
