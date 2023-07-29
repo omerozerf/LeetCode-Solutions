7e17fb08866fecfb432133c46770d28ecaf2df34
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-07-29 10:46:02 +0300

    Update Solution.cs

--- Solutions/1920. Build Array from Permutation/Solution.cs
+++ Solutions/1920. Build Array from Permutation/Solution.cs
@@ -2,13 +2,16 @@
 {
     public class Solution
     {
-        /*
-         
         public int[] BuildArray(int[] nums)
         {
-            
+            int[] ans = new int[nums.Length];
+
+            for (var number = 0; number < ans.Length; number++)
+            {
+                ans[number] = nums[nums[number]];
+            }
+
+            return ans;
         }
-        
-        */
     }
 }
