c0f19ee74516a78f450cc483ba6a6afcccba3dae
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-07-29 11:11:11 +0300

    1470

---
+++ Solutions/1470. Shuffle the Array/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Shuffle_the_Array
+{
+    public class Solution
+    {
+        public int[] Shuffle(int[] numArray, int n)
+        {
+            int[] xArray = new int[n * 2];
+            int[] yArray = new int[n * 2];
+
+            for (int i = 0; i < n; i++)
+            {
+                xArray[i] = numArray[i];
+                yArray[i] = numArray[i + n];
+                
+                int[] answerArray = 
+            }
+        }
+    }
+}
